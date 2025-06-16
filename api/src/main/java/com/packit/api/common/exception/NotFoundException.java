7380317309f55a2e47b9cb15d3bb2fc94878862f
gimgisu <gisu1102@gmail.com>
2025-06-17 03:09:28 +0900

    refactor: 기존 불필요 파일 삭제

--- api/src/main/java/com/packit/api/common/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.packit.api.common.exception;
-
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- api/src/main/java/com/packit/api/domain/user/exception/EmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.packit.api.domain.user.exception;
-
-import com.packit.api.common.exception.BadRequestException;
-
-public class EmailAlreadyExistsException extends BadRequestException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
